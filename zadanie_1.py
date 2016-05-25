fe20dd949e9aa6600df4ae9fd2f1856528498584
Andrey Pronin <ProninA@calipso.com>
2016-05-25 16:09:59 +0300

    Corrected: CamelCase, rename file, count lines, return function

--- zadanie_1.py
+++ zadanie_1.py
@@ -1,30 +1,27 @@
-def select_case(FileName, n=10):
-    import random   #Импортируем модуль random,
-                    #Не разобрался если честно как удобнее использовать numpy
-                    #Но, зато попробовал как новую библиотеку подключать
-    
-    NewFileName = 'all_pairs_res.txt'   #С твоего позволения пока не заморачиваюсь с rename файла
+def select_case(file_name, n=10):
+    import os.path, random
+        
+    new_file_name = os.path.splitext(file_name)[0] + '_res.txt'
     s = []
 
     #Считаем количество строк в исходном файле
     count = 0
-    with open(FileName) as f:
-        for line in f:
-            count += 1
-            
-    #Добавляем немного юмора, тестил на исходном файле текстовом в 70мб
-    if count >= 100: print("Working... Don't reboot pls...")
-    
+    for line in open(file_name).readlines(  ): count += 1
+
+    print('Working...')        
+        
     ch = random.sample(range(count), n) #Выбираем n рандомных номеров строк,
                                         #Позже добавлю контроль ошибки, когда выбираем больше строк чем в файле 
 
-    with open(NewFileName, 'w') as g:
-        with open(FileName) as f:
+    with open(new_file_name, 'w') as g:
+        with open(file_name) as f:
             for i,line in enumerate(f):
                 if i in ch:             #Проверяем если номер строки попадает в "карту рандома ch"
                     g.write(line)       #Добавляю в файл выборки, иначе кидаю в s для последующей перезаписи исходного файла
                 else:
                     s.append(line)
 
-    with open(FileName, 'w') as f:      #Перезаписываю исходный файл
+    with open(file_name, 'w') as f:      #Перезаписываю исходный файл
         f.writelines(s)
+
+    return print('Complete! Look selection in:\n', os.path.abspath(new_file_name))
