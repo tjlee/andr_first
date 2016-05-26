912f10a91c1cadcc98f40911067b325d2de374f0
Andrey Pronin <ProninA@calipso.com>
2016-05-26 16:09:17 +0300

    Improvements on 26/05/2016

--- zadanie_1.py
+++ zadanie_1.py
@@ -1,27 +1,51 @@
-def select_case(file_name, n=10):
-    import os.path, random
+"""
+    Function select_case(file_name, n=10)
+
+    n:  count of random strings from file to cuting,
+        n is facultative parametr, by default n = 10
         
+    file_name:  name of input file with strings
+
+"""
+
+def select_case(file_name, n=10):
+    import os.path, random            
+
     new_file_name = os.path.splitext(file_name)[0] + '_res.txt'
     s = []
 
-    #Считаем количество строк в исходном файле
-    count = 0
-    for line in open(file_name).readlines(  ): count += 1
+    try:
+        # Count of lines in file
+        count = 0
+        for line in open(file_name).xreadlines(  ):
+            count += 1
+            
+        # Select n random line numbers
+        ch = random.sample(range(count), n)
 
-    print('Working...')        
-        
-    ch = random.sample(range(count), n) #Выбираем n рандомных номеров строк,
-                                        #Позже добавлю контроль ошибки, когда выбираем больше строк чем в файле 
+        # Write n random lines to new file
+        # Use ch as list of number lines
+        with open(new_file_name, 'w') as g:
+            with open(file_name) as f:
+                for i,line in enumerate(f):
+                    if i in ch:
+                        g.write(line)
+                    else:
+                        s.append(line)
+        with open(file_name, 'w') as f:
+            f.writelines(s)
 
-    with open(new_file_name, 'w') as g:
-        with open(file_name) as f:
-            for i,line in enumerate(f):
-                if i in ch:             #Проверяем если номер строки попадает в "карту рандома ch"
-                    g.write(line)       #Добавляю в файл выборки, иначе кидаю в s для последующей перезаписи исходного файла
-                else:
-                    s.append(line)
+        print('Complete! Look selection in:\n'+
+              os.path.abspath(new_file_name)
+              )
 
-    with open(file_name, 'w') as f:      #Перезаписываю исходный файл
-        f.writelines(s)
+    # Except case, when user attempts cut more lines,
+    # than is contained in the file.
+    except ValueError:
+        print('Count of lines in '+file_name+' = '+str(count)+
+              "\nselect_case('all_pairs.txt', n),"+
+              "here parametr n must be less than : "+
+              str(count)
+              )
 
-    return print('Complete! Look selection in:\n', os.path.abspath(new_file_name))
+    
