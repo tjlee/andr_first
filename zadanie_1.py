0f79886ea391b44a472b80d062923d463cc562c2
Avpronin <avpronin86@gmail.com>
2016-06-08 14:14:50 +0300

    Improvements on 08/06/2016

--- zadanie_1.py
+++ zadanie_1.py
@@ -10,34 +10,34 @@
 
 def select_case(file_name, n=10):
     import os.path, random, sys
-
+    
+    # Check input file
     if not os.path.isfile(file_name):
-        print('Incorrect name of input file, or file does not exist')
         sys.exit()
-    # Incorrect n
-    if type(n) is not int or n <= 0:
-        print('Incorrect n, type must be integer and n > 0')
+    # Check n
+    if type(n) is not int or n < 0:
+        sys.exit()
+
+    new_file_name = os.path.splitext(file_name)[0] + '_res.txt'
+    
+    if n == 0:
+        open(new_file_name, 'w').close()
+        print (os.path.abspath(new_file_name))
         sys.exit()
         
-    count = 0
-    for line in open(file_name).readlines(  ):
+    count = 0    # count of lines in input file
+    for line in open(file_name).xreadlines():
         count += 1
 
     if n > count:
-        print('Incorrect n, in select_case(file_name, n) 0 < n <', count)
         sys.exit()
-    if n == count:
-        if input('Do you want cut all lines from input file?\
-                 \nprint yes to cuting or another to exit : ')!= 'yes':
-            sys.exit()
 
-    after_cut = []        
-    new_file_name = os.path.splitext(file_name)[0] + '_res.txt'    
+    after_cut = []          
 
     # Select n random line numbers
     ch = random.sample(range(count), n)
 
-    # Write n random lines to new file
+    # Write n random lines to output file
     # Use ch as list of number lines
     with open(new_file_name, 'w') as cut:
         with open(file_name) as until_cut:
@@ -59,12 +59,15 @@ def select_case(file_name, n=10):
         select_case(file_name, n=10) --> cut 10 random lines to output file
 
     2)  Count = 100, n = 100
-        Function request to cut all lines from input to output file
+        Function cut all lines from input to output file
 
-    3)  (n <= 0) or (n > count) or (n not integer)
-        Function repot about incorrect value of parametr n
+    3)  (n < 0) or (n > count) or (n not integer)
+        Function stops working before opening input file to cuting, output file is not created
 
     4)  file_name at select_case(file_name, n=10) incorrect, or does not exist
-        Function repot abut incorrect value of parametr
+        Function stops working before opening input file to cuting, output file is not created
+
+    5)  n = 0
+        Function creates an empty output file and stops before opening input file to cuting
 
 """
